2dd26d4344d2ce7e51e3416c77353f9d8055b43f
Wycliffe Munyes <wyclefmunyes@gmail.com>
2024-07-20 21:09:05 +0300

    Feat: Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import Shop from "./components/Shop";
 import Cart from "./components/Cart";
 import SignUp from "./components/SignUp";
@@ -14,7 +15,8 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Shop />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/signin" element={<SignIn />} />
